3858acb010ec564640d746f0024973a55c74386f
Hanna Lindgren <han.lind@gmail.com>
2018-03-22 15:16:29 +0100

    nfkc

---
+++ scripts/nfkc/nfkc.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/HannaLindgren/go-utils/scripts/util"
+	"golang.org/x/text/unicode/norm"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func process(s string) string {
+	return string(norm.NFKC.Bytes([]byte(s)))
+}
+
+func main() {
+	cmdname := filepath.Base(os.Args[0])
+	if len(os.Args) > 1 && strings.HasPrefix(os.Args[1], "-h") {
+		fmt.Fprintln(os.Stderr, "Utility script for compatibility composition of text data (non-destructive conversion).")
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintf(os.Stderr, "Usage: %s <files>\n", cmdname)
+		fmt.Fprintf(os.Stderr, "       or\n")
+		fmt.Fprintf(os.Stderr, "       cat <file> | %s\n", cmdname)
+		fmt.Fprintf(os.Stderr, "       or\n")
+		fmt.Fprintf(os.Stderr, "       echo <string> | %s\n", cmdname)
+		os.Exit(1)
+	}
+	err := util.ConvertAndPrintFromFileArgsOrStdin(process)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+}
