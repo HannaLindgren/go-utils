c0d78eba11688ce274417e9c9f5f451d72d8f91f
Hanna Lindgren <han.lind@gmail.com>
2019-10-23 15:59:01 +0200

    read file or string from args

--- scripts/util/util.go
+++ scripts/util/util.go
@@ -94,5 +94,32 @@ func ConvertAndPrintFromFilesOrStdin(convert fn, files []string) error {
 
 // ConvertAndPrintFromFileArgsOrStdin takes a conversion function, and as conversion input it uses (1) files specified in os.Args; or (2) stdin. The conversion function should convert an input string to another (output) string. It's a utility for writing simple code for processing textfiles, typically converting each input line into another output line (upcase, line length, etc).
 func ConvertAndPrintFromFileArgsOrStdin(convert fn) error {
-	return ConvertAndPrintFromFilesOrStdin(convert, os.Args[1:])
+	args := os.Args[1:]
+	if len(args) > 0 {
+		for _, arg := range args {
+			if IsFile(arg) {
+				r, fh, err := GetFileReader(arg)
+				defer fh.Close()
+				if err != nil {
+					return err
+				}
+				scanner := bufio.NewScanner(r)
+				for scanner.Scan() {
+					s := scanner.Text()
+					fmt.Println(convert(s))
+				}
+			} else {
+				fmt.Println(convert(arg))
+			}
+
+		}
+	} else {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			s := scanner.Text()
+			fmt.Println(convert(s))
+		}
+	}
+	return nil
+	//return ConvertAndPrintFromFilesOrStdin(convert, os.Args[1:])
 }
