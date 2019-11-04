a4f71fcf0d803786de1cd2e08db20d0d3aad8b4d
Hanna Lindgren <HannaLindgren@users.noreply.github.com>
2019-11-04 23:22:56 +0100

    Update main.go

--- cyr2lat/main.go
+++ cyr2lat/main.go
@@ -73,6 +73,8 @@ var charset = []pair{
 	pair{s1: ".", s2: "."},
 	pair{s1: "?", s2: "?"},
 	pair{s1: "!", s2: "!"},
+	pair{s1: "–", s2: "–"},
+	pair{s1: "-", s2: "-"},
 }
 
 func convert(chsAll []pair, s string) string {
